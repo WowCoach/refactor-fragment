d9a39e0f8ba8b81cd66998efd3f415693a7ff0a7
Waterstrong <sqlin@thoughtworks.com>
2016-01-13 00:52:57 +0800

    [#0 Waterstrong] - extract the regex constant

--- src/main/java/com/water/rover/util/CommandLoader.java
+++ src/main/java/com/water/rover/util/CommandLoader.java
@@ -4,15 +4,16 @@ import static com.water.rover.enumeration.Command.L;
 import static com.water.rover.enumeration.Command.M;
 import static com.water.rover.enumeration.Command.R;
 import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class CommandLoader {
     private static final List<String> VALID_COMMANDS = asList(L.name(), R.name(), M.name());
+    private static final String REGEX = "(?!^)";
 
     public static String[] convertInputIntoCommands(String input) {
-        String[] commandArray = input.split("(?!^)");
+        String[] commandArray = input.split(REGEX);
 
         validateCommands(input, commandArray);
 
@@ -20,7 +21,7 @@ public class CommandLoader {
     }
 
     private static void validateCommands(String input, String[] commandArray) {
-        if (Arrays.stream(commandArray).anyMatch(command -> !VALID_COMMANDS.contains(command))) {
+        if (stream(commandArray).anyMatch(command -> !VALID_COMMANDS.contains(command))) {
             throw new IllegalArgumentException("Invalid command sequence: " + input);
         }
     }
