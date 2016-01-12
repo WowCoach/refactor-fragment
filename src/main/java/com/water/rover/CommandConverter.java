bf079c47279e2c791d8e8800160dd62f27f3fc86
Waterstrong <sqlin@thoughtworks.com>
2016-01-12 23:17:35 +0800

    [#0 Waterstrong] - refactor convert input into command

--- src/main/java/com/water/rover/CommandConverter.java
+++ src/main/java/com/water/rover/CommandConverter.java
@@ -1,4 +1,24 @@
 package com.water.rover;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CommandConverter {
+    public static final List<String> valid_commands = Arrays.asList("L", "R", "M");
+
+    public static String[] convertInputIntoCommands(String input) {
+        String[] commandArray = input.split("(?!^)");
+
+        validateCommands(input, commandArray);
+
+        return commandArray;
+    }
+
+    private static void validateCommands(String input, String[] commandArray) {
+        for (String command : commandArray) {
+            if (!valid_commands.contains(command)) {
+                throw new IllegalArgumentException("Invalid command sequence: " + input);
+            }
+        }
+    }
 }
--- src/main/java/com/water/rover/MarsRover.java
+++ src/main/java/com/water/rover/MarsRover.java
@@ -1,10 +1,8 @@
 package com.water.rover;
 
-import java.util.Arrays;
-import java.util.List;
+import static com.water.rover.CommandConverter.*;
 
 public class MarsRover {
-    public static final List<String> valid_commands = Arrays.asList("L", "R", "M");
 
     private Location location;
 
@@ -17,47 +15,19 @@ public class MarsRover {
 
         for (String command : commands) {
             if (command.equals("M")) {
-                move();
+                location.move();
             } else if (command.equals("R")) {
-                turnRight();
+                location.turnRight();
             } else if (command.equals("L")) {
-                turnLeft();
+                location.turnLeft();
             }
         }
 
         return asString();
     }
 
-    private void move() {
-        location.move();
-    }
-
     private String asString() {
         return location.asString();
-    }
-
-    private void turnLeft() {
-        location.turnLeft();
-    }
-
-    private void turnRight() {
-        location.turnRight();
-    }
-
-    private static String[] convertInputIntoCommands(String input) {
-        String[] commandArray = input.split("(?!^)");
-
-        validateCommands(input, commandArray);
-
-        return commandArray;
-    }
-
-    private static void validateCommands(String input, String[] commandArray) {
-        for (String command : commandArray) {
-            if (!valid_commands.contains(command)) {
-                throw new IllegalArgumentException("Invalid command sequence: " + input);
-            }
-        }
     }
 
 }
